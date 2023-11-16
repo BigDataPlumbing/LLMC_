bbdacb2fd704deef735a397edeae1e75f0f4f68c
Big Data Plumbing <142255006+BigDataPlumbing@users.noreply.github.com>
2023-11-16 11:53:36 -0300

    txt_db.py

--- txt_db.py
+++ txt_db.py
@@ -94,7 +94,7 @@ def data_to_firestore(message):
             return credentials.Certificate(cert_path), project_id
         
         
-        if cert_path is not None and message is not None:
+        if uploaded_file is not None and message is not None:
 
                     # Display the result or perform further actions
             st.write("Certificate:", cert_path)
